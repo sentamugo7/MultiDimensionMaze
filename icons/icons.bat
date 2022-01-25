45e62df0e820ad2e886ba65d7a03d74beebaa368
sentamugo7 <sentamugo7@gmail.com>
2022-01-25 15:30:21 -0800

    Create icons.bat

---
+++ icons/icons.bat
@@ -0,0 +1,44 @@
+magick convert -background none -colorspace sRGB -define png:color-type=6 up_yes.svg up_yes.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 up_no.svg up_no.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 down_yes.svg down_yes.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 down_no.svg down_no.png
+
+magick convert -background none -colorspace sRGB -define png:color-type=6 left_yes.svg left_yes.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 left_no.svg left_no.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 right_yes.svg right_yes.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 right_no.svg right_no.png
+
+magick convert -background none -colorspace sRGB -define png:color-type=6 reverse_yes.svg reverse_yes.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 reverse_no.svg reverse_no.png
+
+magick convert -background none -colorspace sRGB -define png:color-type=6 dimensions.svg dimensions.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 settings.svg settings.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 MultiDimensionalMaze.svg MultiDimensionalMaze.png
+
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_u_plus.svg hint_u_plus.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_u_minus.svg hint_u_minus.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_v_plus.svg hint_v_plus.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_v_minus.svg hint_v_minus.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_w_plus.svg hint_w_plus.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_w_minus.svg hint_w_minus.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_up.svg hint_up.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_down.svg hint_down.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_left.svg hint_left.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_right.svg hint_right.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_forward.svg hint_forward.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_reverse.svg hint_reverse.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_blank.svg hint_blank.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 hint_win.svg hint_win.png
+
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_south.svg orientation_south.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_east.svg orientation_east.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_north.svg orientation_north.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_west.svg orientation_west.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_up_south.svg orientation_up_south.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_up_east.svg orientation_up_east.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_up_north.svg orientation_up_north.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_up_west.svg orientation_up_west.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_down_south.svg orientation_down_south.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_down_east.svg orientation_down_east.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_down_north.svg orientation_down_north.png
+magick convert -background none -colorspace sRGB -define png:color-type=6 orientation_down_west.svg orientation_down_west.png
